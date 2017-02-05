ee94a52727803b5def76a0763cf975fbb4c863a1
Haim Yaakov <haimyyy@haimyaakov.dev.capriza.com>
2017-02-05 19:20:14 +0200

    add 3 inputs

--- src/container/SidebarContainer.js
+++ src/container/SidebarContainer.js
@@ -6,13 +6,19 @@ import RaisedButton from "material-ui/RaisedButton";
 import Drawer from "material-ui/Drawer";
 import BlazMenu from "../components/BlazMenu";
 import Paper from 'material-ui/Paper';
+import TextField from 'material-ui/TextField';
+
 class SidebarContainer extends React.Component {
     constructor() {
         super();
         this.state = {
-            pageHeight: 0
+            pageHeight: 0,
+            thirdInput:0,
+            firstInput:0,
+            secInput:0
         };
         this.handleToggle = this.handleToggle.bind(this)
+        this.calculate = this.calculate.bind(this)
     }
     componentWillMount() {
         this.props.actions.loadData()
@@ -24,6 +30,11 @@ class SidebarContainer extends React.Component {
         //found screen height and update the component
         this.setState({pageHeight: window.innerHeight - 20})
     }
+    calculate() {
+        console.log(this.state.firstInput)
+        console.log(this.state.secInput)
+        console.log(this.state.thirdInput)
+    }
     render() {
         let styles = {
             container: {
@@ -34,19 +45,35 @@ class SidebarContainer extends React.Component {
                 textAlign: 'center',
                 display: 'inline-block',
                 height: this.state.pageHeight // body margin
+            },
+            paper2: {
+                width: '50%',
+                marginRight: '30%',
+                marginLeft: '30%',
+                marginTop: 30,
+                textAlign: 'center',
+                height: 700,  // body margin
+                paddingTop:30
+            },
+            input:{
+                fontSize: 14,
+            },
+            labelStyle:{
+                marginLeft: 5
             }
         }
+
         // menu items
         let blazMenu = this.props.sidebar.isDisplayed ? <Paper style={styles.paper} zDepth={2} >
-            <BlazMenu
-                reports={this.props.sidebar.reports}
-                updateSearchValue={this.props.actions.updateSearchValue}
-                searchValue={this.props.sidebar.searchValue}
-                pageHeight={this.state.pageHeight}
-                updateSidebarView={this.props.actions.updateSidebarView}
-                loadData={this.props.actions.loadData}
-            />
-        </Paper> : null
+                <BlazMenu
+                    reports={this.props.sidebar.reports}
+                    updateSearchValue={this.props.actions.updateSearchValue}
+                    searchValue={this.props.sidebar.searchValue}
+                    pageHeight={this.state.pageHeight}
+                    updateSidebarView={this.props.actions.updateSidebarView}
+                    loadData={this.props.actions.loadData}
+                />
+            </Paper> : null
 
         return (
             <div style={styles.container}>
@@ -57,6 +84,34 @@ class SidebarContainer extends React.Component {
                         onTouchTap={this.handleToggle}
                     />
                     {blazMenu}
+                    <Paper style={styles.paper2}>
+                        <TextField
+                            ref={node=> {this.firstInput = node}}
+                            onChange={(node, value)=>(this.setState({firstInput:value}))}
+                            style={styles.input}
+                            hintStyle={styles.labelStyle}
+                            inputStyle={styles.labelStyle}
+                            hintText="enter number"/>
+                        <TextField
+                            onChange={(node, value)=>(this.setState({secInput:value}))}
+                            ref={node=> {this.secInput = node}}
+                            style={styles.input}
+                            hintStyle={styles.labelStyle}
+                            inputStyle={styles.labelStyle}
+                            hintText="enter number"/>
+                        <TextField
+                            onChange={(node, value)=>(this.setState({thirdInput:value}))}
+                            ref={node=> {this.thirdInput = node}}
+                            style={styles.input}
+                            hintStyle={styles.labelStyle}
+                            inputStyle={styles.labelStyle}
+                            hintText="enter number"/>
+                    </Paper>
+                    <RaisedButton
+                        label={"calculate"}
+                        style={{float:'none'}}
+                        onTouchTap={this.calculate}
+                    />
                 </div>
             </div>
 
